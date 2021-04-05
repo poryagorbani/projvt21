fc52410fe51f8406d92229d2513263430e2a9da4
poryagorbani <71005697+poryagorbani@users.noreply.github.com>
2021-04-05 05:19:29 +0200

    SISTA PUSH

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // Denna fil skall innehålla JavaScript (JS)
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".nav").addClass("sticky");
+        }else{
+            $(".nav").removeClass("sticky");
+        }
+    })
+
+    $(".knapp").click(function(){
+        $(".nav .meny").toggleClass("active")
+    });
+});
